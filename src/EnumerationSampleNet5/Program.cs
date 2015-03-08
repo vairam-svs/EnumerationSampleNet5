5a2adf0d51439605435d396ac461b6ba02a6c3c3
vairam-svs <vairam.svs@outlook.com>
2015-03-07 23:31:23 -0800

    Sample Enumeration using Core and different packages for each assembly

---
+++ src/EnumerationSampleNet5/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EnumerationSampleNet5
+{
+    public class Program
+    {
+        public void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+            List<string> titles = new List<string>() { "raja", "rani", "mandiri" };
+
+            //lambda sample
+            titles.ForEach(x => Console.WriteLine(x));
+            Console.WriteLine("Is a collection of titles in a kingdom");
+
+            // Display the contents of the list using the Print method.
+            Console.WriteLine("Can also print using a function");
+            titles.ForEach(Print);
+
+            // The following demonstrates the anonymous method feature of C# 
+            // to display the contents of the list to the console.
+            Console.WriteLine("Can also print using a delegate");
+            titles.ForEach(delegate (string title)
+            {
+                Console.WriteLine(title);
+            });
+            Console.Read();
+        }
+
+        private static void Print(string s)
+        {
+            Console.WriteLine(s);
+        }
+    }
+}
